812a8017476a2686f8ec212168ca148d247fac84
annamburke800-hub <annamburke800@gmail.com>
2025-09-20 12:20:00 -0600

    finished prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first Nname?");
+        Console.Write("What is your first Nname? ");
         string fname = Console.ReadLine();
 
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
         Console.WriteLine();
         Console.Write($"My name is {lname}, {fname} {lname}.");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,64 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What score did you get? ");
+        string sscore = Console.ReadLine();
+        int score = Convert.ToInt32(sscore);
+        string letterGrade;
+        string porm;
+        // porm stands for Plus OR Minus
+
+        if (score > 93 || score < 60)
+        {
+            porm = "";
+        }
+        else if (score % 10 >= 7)
+        {
+            porm = "+";
+        }
+        else if (score % 10 <= 3)
+        {
+            porm = "-";
+        }
+        else
+        {
+            porm = "";
+        }
+
+        if (score >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (score >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (score >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (score >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You scored a {score}, so your grade is {letterGrade}{porm}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
     }
 }
