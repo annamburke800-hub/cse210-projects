cec7d47f9074d3ac93a8a6acba23cfe5f08404a5
annamburke800-hub <annamburke800@gmail.com>
2025-09-20 11:58:31 -0600

    completing prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first Nname?");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string lname = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write($"My name is {lname}, {fname} {lname}.");
     }
 }
