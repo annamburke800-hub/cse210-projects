b2575cc7bcabc93d7c18aa47787fc64cdd110ac1
annamburke800-hub <annamburke800@gmail.com>
2025-09-23 13:27:48 -0600

    Completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        while (true)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            int guess = -5;
+            int i = 0;
+            // Console.WriteLine($"Number: {number}");
+
+            while (guess != number)
+            {
+                i++;
+                Console.Write("What is your guess? ");
+                string guesss = Console.ReadLine();
+                guess = Convert.ToInt32(guesss);
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher!");
+                }
+            }
+
+            Console.WriteLine($"Congratulations! You guessed the number in {i} guess(es)!");
+            Console.WriteLine($"The number was {number}");
+
+            Console.Write("Play again? (y/n): ");
+            string yorn = Console.ReadLine();
+            string lowyorn = yorn.ToLower();
+            if (lowyorn == "n")
+            {
+                break;
+            }
+        }
+        Console.WriteLine("Thanks for playing!");
     }
 }
